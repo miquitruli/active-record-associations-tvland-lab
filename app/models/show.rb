34627787b27053ed849d80cc5aff3f571eb5bf42
Micaella Colombo <micaelc@g.clemson.edu>
2021-09-29 18:10:29 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actors.map do |c|
+        c.full_name
+    end
+  end
 end
+
